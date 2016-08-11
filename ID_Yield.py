cefeddd4fd8d34c5aeb7b552f07f693cd59690c1
laurencechan <laurencechan.qq.com>
2016-08-11 09:59:46 +0800

    身份证最后一位验证码

--- ID_Yield.py
+++ ID_Yield.py
@@ -63,7 +63,7 @@ class ID_Yield():
                         lev3_list.append(dict_6)
                         # dict_6也是一个元素为字典的列表 字典格式为 [{231222:"黑龙江省绥化市兰西县"},{330424:"浙江省嘉兴市海盐县"}]
         xx = random.choice(lev3_list)
-        # print int(xx.keys()[0])
+        # print int(xx.values()[0])
         return xx
 
     @classmethod
@@ -108,7 +108,7 @@ class ID_Yield():
             sum = sum + 2*int(birthday[0])+1*int(birthday[1])+6*int(birthday[2])+3*int(birthday[3])+7*int(birthday[4])+9*int(birthday[5])+10*int(birthday[6])+5*int(birthday[7])
             last_three = ID_Yield.last_three()
             sum = sum + 8*int(last_three[0])+4*int(last_three[1])+2*int(last_three[2])
-            yushu = sum % 11
+            yushu = sum % 11   # 身份证最后一位 验证码  http://baike.baidu.com/view/188003.htm?qq-pf-to=pcqq.c2c#1_7
             if yushu == 0:                      #7－9－10－5－8－4－2－1－6－3－7－9－10－5－8－4－2
                 last_one = "1"                  #0－1－2－3－4－5－6－7－8－9－10
             elif yushu == 1:                    #1－0－X －9－8－7－6－5－4－3－2
